61e9f802eaa16c4321051bab26654b136d45af0c
ivarvani <i.varvani@gmail.com>
2023-06-17 15:58:40 +1000

    PyBank working Pypoll need Modification

---
+++ PyPoll/main.py
@@ -0,0 +1,50 @@
+"""
+election data 
+"""
+# importing os and csv module
+import os
+import csv
+
+# defining the path of the csv file and opening it in read format
+election_data_path = os.path.join("Resources", "election_data.csv")
+with open(election_data_path, "r", encoding="UTF-8") as electionfile:
+    input_data = csv.reader(electionfile, delimiter=",")
+    header = next(input_data)  # column data headers
+    # print(header)
+    candidate_list = []  # creating the list for candidates in the election
+    vote_data = []  # creating the list for votes for canidates
+    for row in input_data:
+        vote_data.append(row[2])
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+    print(f'candidates : {" , ".join(candidate_list)}')
+    # print(data)
+    total_votes = len(vote_data)
+    winner = 0 
+    candidates_results = [] # the list will be populated by strings
+    for candidate in candidate_list:
+        candidate_count = vote_data.count(candidate)
+        if candidate_count > winner:
+            winner = candidate_count
+            winner_name = candidate
+        # print(
+        #     f"{candidate}:   {vote_data.count(candidate)}: {(((vote_data.count(candidate))/total_votes)*100):.3f}%"
+        # )
+        candidate_result: (str)=  f"{candidate}:   {vote_data.count(candidate)}: {(((vote_data.count(candidate))/total_votes)*100):.3f}%"
+        # ":(str)" has no effect on code. its to tell us the variable is string
+        candidates_results.append(candidate_result)
+        print(candidates_results)
+    print(f"The total votes cast: {total_votes}")
+    print(winner)
+    print(winner_name)
+
+
+    # writing the results to analysis
+    text_file_path = "Analysis/Analysis.txt"
+    with open(text_file_path, "w", encoding="UTF-8") as text_file:
+        text_file.write("PyPoll Analysis")
+        text_file.write("\n----------------------------------------")
+        text_file.write("\n" f"The total votes cast: {total_votes}")
+        text_file.write("\n----------------------------------------")
+        text_file.write("\n")
+        text_file.write("\n".join(candidates_results))
---
+++ Pybank/main.py
@@ -0,0 +1,62 @@
+"""
+csv module
+"""
+#imported csv and os modules
+import os
+
+import csv
+
+budget_data_path = os.path.join("Resources","budget_data.csv")
+with open(budget_data_path, encoding="UTF-8") as csv_file:
+    input_data = csv.reader(csv_file, delimiter=",")
+    headers = next(input_data) # this is our columns headers row
+    line_count = 0  # number of rows in our data whuch eqauls to the months
+    total = 0  # the net earned
+    difference = 0
+    difference_list = [] # list  to get the changes across the entire period
+    for row in input_data:
+        if line_count == 0:
+            line_count += 1
+            total = int(row[1])
+            row_value_before = int(row[1])
+            highest_change = 0
+            lowest_change = 0
+            difference = 0
+            count_of_difference = 0
+        else:
+            # print(f'{" , ".join(row)}')
+            line_count += 1
+            total += int(row[1])
+            difference = int(row[1]) - (row_value_before)
+            difference_list.append(difference)
+            count_of_difference += 1
+            #print(f'{",".join(row)}   {difference}')
+            row_value_before = int(row[1])
+            if difference > highest_change:
+                highest_change = difference
+                month = row[0]
+            if difference < lowest_change:
+                lowest_change = difference
+                new_month = row[0]
+
+    #printing the results on the terminal
+    months = f"number of months = {line_count }"
+    print (months)
+    net = f"net total = {total}"
+    print(net)
+    positive_change = f"greatest positive change :{month}  {highest_change}"
+    print(positive_change)
+    negative_change =f"greatest negative change :{new_month}  {lowest_change}"
+    print(negative_change)
+    average = (sum(difference_list)/len(difference_list))
+    print(f'the average change : {average :.2f}')
+    #writing the results to analysis
+    text_file_path ="Analysis/Analysis.txt"
+    with open(text_file_path,'w',encoding='UTF-8') as text_file:
+        text_file.write("PyBank Analysis")
+        text_file.write("\n----------------------------------------")
+        text_file.write("\n" + months)
+        text_file.write("\n"+ net)
+        text_file.write("\n" + positive_change)
+        text_file.write("\n" + negative_change)
+        text_file.write("\n" f'the average change : {average :.2f}')
